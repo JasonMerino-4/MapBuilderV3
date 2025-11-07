3edc91b451fe81b3008667ebd567c58721086174
JasonMerino-4 <jasonmerino29@gmail.com>
2025-11-07 14:12:15 -0600

    Updated drawLin function to go to the center of the the pin node rather then the top left corner.

--- script.js
+++ script.js
@@ -113,9 +113,20 @@ document.addEventListener("DOMContentLoaded", function () {
             return parseInt(this.pinElement.style.top) || 0;
         }
 
+        getIntYCenterPosition(){
+            let yPos = parseInt(this.pinElement.style.top) || 0;
+            return yPos + (parseInt(this.pinElement.clientHeight)/2);
+        }
+
         getIntXPosition() {
             return parseInt(this.pinElement.style.left) || 0;
         }
+
+        getIntXCenterPosition(){
+            let xPos = parseInt(this.pinElement.style.left) || 0;
+            return xPos + (parseInt(this.pinElement.clientWidth)/2);
+        }
+
 
         pinToJSON() {
             let temp = {
@@ -328,10 +339,10 @@ document.addEventListener("DOMContentLoaded", function () {
     function addLine(pin1, pin2){
         let newLine = document.createElementNS("http://www.w3.org/2000/svg", "line")
 
-        newLine.setAttribute("x1", pin1.getIntXPosition());
-        newLine.setAttribute("y1", pin1.getIntYPosition());
-        newLine.setAttribute("x2", pin2.getIntXPosition());
-        newLine.setAttribute("y2", pin2.getIntYPosition());
+        newLine.setAttribute("x1", pin1.getIntXCenterPosition());
+        newLine.setAttribute("y1", pin1.getIntYCenterPosition());
+        newLine.setAttribute("x2", pin2.getIntXCenterPosition());
+        newLine.setAttribute("y2", pin2.getIntYCenterPosition());
         newLine.setAttribute("stroke", "black");
 
         mapPathsSVG.appendChild(newLine);
